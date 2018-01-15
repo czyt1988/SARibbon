49f169a19014e4b485750b8fbede543b277f293f
czyt1988 <czy.t@163.com>
2018-01-15 17:01:56 +0800

    修改样式

--- src/SARibbonBar/SARibbonSeparatorWidget.cpp
+++ src/SARibbonBar/SARibbonSeparatorWidget.cpp
@@ -1,6 +1,7 @@
 ﻿#include "SARibbonSeparatorWidget.h"
 #include <QStylePainter>
 #include <QPainter>
+#include <QDebug>
 SARibbonSeparatorWidget::SARibbonSeparatorWidget(int height, QWidget *parent)
     :QWidget(parent)
 {
@@ -22,6 +23,7 @@ void SARibbonSeparatorWidget::paintEvent(QPaintEvent *event)
     Q_UNUSED(event);
     QPainter painter(this);
     int margin = 3;
+    //qDebug() << palette().background().color().darker(114).name();
     painter.setPen(palette().background().color().darker(114));
     int x1 = rect().center().x();
     painter.drawLine(QPoint(x1, rect().top() + margin), QPoint(x1, rect().bottom() - margin));
