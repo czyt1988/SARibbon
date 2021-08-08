01be23def19672dfac0196592fe49caed34398ce
尘中远 <czy.t@163.com>
2021-08-08 17:24:53 +0800

    修正自定义显示错误问题，对未设置object的变量生成警告

---
+++ src/SARibbonBar/SARibbonLineWidgetContainer.cpp
@@ -0,0 +1,53 @@
+#include "SARibbonLineWidgetContainer.h"
+#include <QHBoxLayout>
+
+SARibbonLineWidgetContainer::SARibbonLineWidgetContainer(QWidget *par) : QWidget(par)
+    , m_innerWidget(nullptr)
+{
+    m_labelPrefix = new QLabel(this);
+    m_labelSuffix = new QLabel(this);
+    QHBoxLayout *lay = new QHBoxLayout();
+
+    lay->setMargin(0);
+    lay->setSpacing(0);
+    lay->addWidget(m_labelPrefix);
+    lay->addWidget(m_labelSuffix);
+    setLayout(lay);
+}
+
+
+void SARibbonLineWidgetContainer::setWidget(QWidget *innerWidget)
+{
+    QHBoxLayout *lay = static_cast<QHBoxLayout *>(layout());
+
+    if (m_innerWidget) {
+        lay->replaceWidget(m_innerWidget, innerWidget);
+    }else{
+        lay->insertWidget(1, innerWidget);
+    }
+    m_innerWidget = innerWidget;
+}
+
+
+void SARibbonLineWidgetContainer::setPrefix(const QString& str)
+{
+    m_labelPrefix->setText(str);
+}
+
+
+void SARibbonLineWidgetContainer::setSuffix(const QString& str)
+{
+    m_labelSuffix->setText(str);
+}
+
+
+QLabel *SARibbonLineWidgetContainer::labelPrefix() const
+{
+    return (m_labelPrefix);
+}
+
+
+QLabel *SARibbonLineWidgetContainer::labelSuffix() const
+{
+    return (m_labelSuffix);
+}
---
+++ src/SARibbonBar/SARibbonLineWidgetContainer.h
@@ -0,0 +1,41 @@
+#ifndef SARIBBONLINEWIDGETCONTAINER_H
+#define SARIBBONLINEWIDGETCONTAINER_H
+
+#include <QtCore/qglobal.h>
+#include <QWidget>
+#include <QLabel>
+#include "SARibbonGlobal.h"
+
+/**
+ * @brief 一个窗口容器，把窗口放置中间，前面后面都可以设置文本，主要用于放置在pannel上的小窗口
+ *
+ * 实现如下效果：
+ *
+ * PrefixLabel|_Widget_|SuffixLabel
+ *
+ */
+class SA_RIBBON_EXPORT SARibbonLineWidgetContainer : public QWidget
+{
+public:
+    SARibbonLineWidgetContainer(QWidget *par = nullptr);
+    //设置widget,不允许设置一个nullptr
+    void setWidget(QWidget *innerWidget);
+
+    //设置前缀
+    void setPrefix(const QString& str);
+
+    //设置后缀
+    void setSuffix(const QString& str);
+
+    //前缀文本框
+    QLabel *labelPrefix() const;
+    QLabel *labelSuffix() const;
+
+private:
+    //两个文本
+    QLabel *m_labelPrefix;
+    QLabel *m_labelSuffix;
+    QWidget *m_innerWidget;
+};
+
+#endif // SARIBBONWIDGETCONTAINER_H
