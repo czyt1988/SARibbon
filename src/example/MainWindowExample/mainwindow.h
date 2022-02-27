3101bac0e83e307b1416ca51d621d84eed5df2f5
尘中远 <czy.t@163.com>
2022-02-27 23:35:28 +0800

    添加两个信号 修改例子内容

--- src/example/MainWindowExample/mainwindow.h
+++ src/example/MainWindowExample/mainwindow.h
@@ -10,37 +10,38 @@ class MainWindow : public SARibbonMainWindow
 {
     Q_OBJECT
 public:
-    MainWindow(QWidget *par = nullptr);
-private slots:
-    void onShowContextCategory(bool on);
-    void onStyleClicked(int id);
-
+    MainWindow(QWidget* par = nullptr);
 
 private:
-    void createCategoryMain(SARibbonCategory *page);
-    void createCategoryOther(SARibbonCategory *page);
-    void createCategoryDelete(SARibbonCategory *page);
-    void createContextCategoryPage1(SARibbonCategory *page);
-    void createContextCategoryPage2(SARibbonCategory *page);
+    void createCategoryMain(SARibbonCategory* page);
+    void createCategoryOther(SARibbonCategory* page);
+    void createCategoryDelete(SARibbonCategory* page);
+    void createCategorySize(SARibbonCategory* page);
+    void createContextCategory1();
+    void createContextCategory2();
+    void createContextCategoryPage1(SARibbonCategory* page);
+    void createContextCategoryPage2(SARibbonCategory* page);
     void addSomeOtherAction();
-
+    QAction* createAction(const QString& text, const QString& iconurl, const QString& objName);
+    QAction* createAction(const QString& text, const QString& iconurl);
 private slots:
     void onMenuButtonPopupCheckableTest(bool b);
     void onInstantPopupCheckableTest(bool b);
     void onDelayedPopupCheckableTest(bool b);
-
     void onMenuButtonPopupCheckabletriggered(bool b);
     void onInstantPopupCheckabletriggered(bool b);
     void onDelayedPopupCheckabletriggered(bool b);
+    void onShowContextCategory(bool on);
+    void onStyleClicked(int id);
 
 private:
-    SARibbonContextCategory *m_contextCategory;
-    SARibbonContextCategory *m_contextCategory2;
-    SARibbonCustomizeWidget *m_customizeWidget;
-    QTextEdit *m_edit;
-    SARibbonActionsManager *m_actMgr;
+    SARibbonContextCategory* m_contextCategory;
+    SARibbonContextCategory* m_contextCategory2;
+    SARibbonCustomizeWidget* m_customizeWidget;
+    QTextEdit* m_edit;
+    SARibbonActionsManager* m_actMgr;
     int m_actionTagText;
     int m_actionTagWithIcon;
 };
 
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
