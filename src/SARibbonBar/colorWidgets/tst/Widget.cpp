70ef4a9af1dd27e44537895a80381cb0e162b8f6
czy <czy.t@163.com>
2023-04-17 23:35:13 +0800

    开展colorWidgets的工作

---
+++ src/SARibbonBar/colorWidgets/tst/Widget.cpp
@@ -0,0 +1,16 @@
+﻿#include "Widget.h"
+#include "ui_Widget.h"
+
+Widget::Widget(QWidget* parent) : QWidget(parent), ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    ui->toolButtonIconOnlyNoIcon->setColor(Qt::blue);
+    ui->toolButtonIconOnlyHaveIcon->setColor(Qt::blue);
+    ui->toolButtonIconOnlyHaveIconSmall->setColor(Qt::blue);
+    ui->toolButtonIconOnlyHaveIconLarge->setColor(Qt::blue);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
