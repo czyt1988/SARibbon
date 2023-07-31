df2bb1d0c927b49c9f9fc7035e0db9a3a3f4e98b
czy <czy.t@163.com>
2023-07-31 22:19:14 +0800

    完善simpleexample

--- src/example/SimpleExample/MainWindow.cpp
+++ src/example/SimpleExample/MainWindow.cpp
@@ -1,7 +1,22 @@
 ﻿#include "MainWindow.h"
-
+#include "SARibbon.h"
 MainWindow::MainWindow(QWidget* parent) : SARibbonMainWindow(parent)
 {
+    setWindowIcon(QIcon(":/app/icon/SA.svg"));
+    SARibbonBar* bar = ribbonBar();
+    bar->applicationButton()->setText(tr("  File  "));
+    SARibbonCategory* mainCate = bar->addCategoryPage(tr("Main"));
+    SARibbonPannel* pannel     = mainCate->addPannel(tr("actions"));
+    pannel->addAction(tr("action1"), QIcon(":/app/icon/action.svg"), QToolButton::InstantPopup);
+    pannel->addAction(tr("action2"), QIcon(":/app/icon/customize0.svg"), QToolButton::InstantPopup);
+    pannel->addAction(tr("action3"), QIcon(":/app/icon/save.svg"), QToolButton::InstantPopup);
+    pannel->addAction(tr("action4"), QIcon(":/app/icon/item.svg"), QToolButton::InstantPopup, SARibbonPannelItem::Small);
+    pannel->addAction(tr("action5"), QIcon(":/app/icon/folder-star.svg"), QToolButton::InstantPopup, SARibbonPannelItem::Small);
+    pannel->addAction(tr("action6"), QIcon(":/app/icon/test1.svg"), QToolButton::InstantPopup, SARibbonPannelItem::Small);
+    SARibbonCategory* otherCate = bar->addCategoryPage(tr("Other"));
+    Q_UNUSED(otherCate);
+    resize(800, 600);
+    // more example see MainWindowExample
 }
 
 MainWindow::~MainWindow()
