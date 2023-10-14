7dab6238073ce11c74c46dc511165f92bf766ccc
czyt1988 <czy.t@163.com>
2023-10-14 23:33:57 +0800

    添加变更日志

--- src/SARibbonBar/SARibbonGlobal.h
+++ src/SARibbonBar/SARibbonGlobal.h
@@ -3,53 +3,78 @@
 #include <qglobal.h>
 #include <memory>
 
-//! 版本记录：
-//!
-//! -----1.x版本的为引入第三方库frameless，需要c++17支持，qt5.14以上
-//! -----0.x版本的为没有引入第三方库frameless，仅仅对windows进行适配，c++11即可，qt5.8以上
-//! 2023-10-09 -> 0.7.1
-//! 增加了深色主题
-//! 调整了枚举的命名方式
-//! 增加了一些重绘方式
-//!
-//! 2023-10-09 -> 0.7.0
-//! 修正了category的布局问题
-//!
-//! 2023-09-27 -> 0.6.0
-//! 添加了Amalgamate，修正了一些显示的bug，修正cmake的异常
-//! 添加了上下文标签中category标题名字改变的信号
-//!
-//! 2023-05-28 -> 0.5.0
-//! 调整了大按钮模式下的显示方案，去除了原来SARibbonToolButton的Lite和Normal模式，以WordWrap来表征
-//! 支持文字自定义换行
-//! 调整了RibbonPannel的标题栏的高度计算方案
-//!
-//! 0.5.1
-//! 不使用QString::simplified,而是简单的仅仅替换\n的simplified，这样中文换行不会多出空格
-//!
-//! 0.5.2
-//! SARibbonColorToolButton\SARibbonToolButton修正&操作在三项表达式未加括号问题
-//! SARibbonStyleOption添加虚析构函数
-//! 原来SARibbonElementCreateDelegate类改名为SARibbonElementFactory
-//!
+/**
+ * @file  SARibbonGlobal.h
+ * 
+ * 版本记录：
+ * change log
+ * 
+ * 2023-10-14 -> 1.0.1
+ * cn:
+ * 引入第三方库frameless
+ * 支持ubuntu和macos
+ * 解决windwos系统的多屏幕移动问题
+ * 依赖frameless，实现windows系统的一些界面操作，如边缘触发半屏
+ * 添加深色主题
+ * 优化了一些界面刷新逻辑
+ * 最低C++版本要求为c++17
+ * 一些接口的调整和一些枚举名称的调整
+ * en:
+ * import third-party library:frameless
+ * Supports ubuntu and macos
+ * Solving the problem of multi screen movement in Windwos system
+ * Relying on frameless library to implement some interface operations in Windows systems, such as edge triggered half screen
+ * Add a dark theme
+ * Optimized some interface refresh logic
+ * The minimum C++version requirement is c++17
+ * Some interface adjustments and some enumeration name adjustments
+ * 
+ * -----1.x版本的为引入第三方库frameless，需要c++17支持，qt5.14以上
+ * -----0.x版本的为没有引入第三方库frameless，仅仅对windows进行适配，c++11即可，qt5.8以上
+ * 2023-10-09 -> 0.7.1
+ * 增加了深色主题
+ * 调整了枚举的命名方式
+ * 增加了一些重绘方式
+ * 
+ * 2023-10-09 -> 0.7.0
+ * 修正了category的布局问题
+ * 
+ * 2023-09-27 -> 0.6.0
+ * 添加了Amalgamate，修正了一些显示的bug，修正cmake的异常
+ * 添加了上下文标签中category标题名字改变的信号
+ * 
+ * 2023-05-28 -> 0.5.0
+ * 调整了大按钮模式下的显示方案，去除了原来SARibbonToolButton的Lite和Normal模式，以WordWrap来表征
+ * 支持文字自定义换行
+ * 调整了RibbonPannel的标题栏的高度计算方案
+ * 
+ * 0.5.1
+ * 不使用QString::simplified,而是简单的仅仅替换\n的simplified，这样中文换行不会多出空格
+ * 
+ * 0.5.2
+ * SARibbonColorToolButton\SARibbonToolButton修正&操作在三项表达式未加括号问题
+ * SARibbonStyleOption添加虚析构函数
+ * 原来SARibbonElementCreateDelegate类改名为SARibbonElementFactory
+ */
+
 
 /**
  * @def ribbon的数字版本 {MAJ}.MIN.PAT
  */
 #ifndef SA_RIBBON_BAR_VERSION_MAJ
-#define SA_RIBBON_BAR_VERSION_MAJ 0
+#define SA_RIBBON_BAR_VERSION_MAJ 1
 #endif
 /**
  * @def ribbon的数字版本 MAJ.{MIN}.PAT
  */
 #ifndef SA_RIBBON_BAR_VERSION_MIN
-#define SA_RIBBON_BAR_VERSION_MIN 7
+#define SA_RIBBON_BAR_VERSION_MIN 0
 #endif
 /**
  * @def ribbon的数字版本 MAJ.MIN.{PAT}
  */
 #ifndef SA_RIBBON_BAR_VERSION_PAT
-#define SA_RIBBON_BAR_VERSION_PAT 0
+#define SA_RIBBON_BAR_VERSION_PAT 1
 #endif
 
 /**
