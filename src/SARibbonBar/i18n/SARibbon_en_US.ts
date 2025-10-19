cfafe4275a72d90986360628c61b4f2b5379f7c5
czyt1988 <czy.t@163.com>
2025-10-19 19:30:47 +0800

    添加翻译

---
+++ src/SARibbonBar/i18n/SARibbon_en_US.ts
@@ -0,0 +1,281 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="336"/>
+        <source>SARibbon Warning !!! customize rename category,but get an empty category object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="360"/>
+        <source>SARibbon Warning !!! customize rename panel,but get an empty category/panel object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="383"/>
+        <source>SARibbon Warning !!! customize change category order,but get an empty category object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="407"/>
+        <source>SARibbon Warning !!! customize change panel order,but get an empty category/panel object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="436"/>
+        <source>SARibbon Warning !!! customize change action order,but get an empty category/panel/action object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="458"/>
+        <source>SARibbon Warning !!! customize remove category,but get an empty category object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="479"/>
+        <source>SARibbon Warning !!! customize remove panel,but get an empty category/panel object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="505"/>
+        <source>SARibbon Warning !!! customize remove action,but get an empty category/panel/action object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="527"/>
+        <source>SARibbon Warning !!! customize visible category,but get an empty category object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SAColorMenu</name>
+    <message>
+        <location filename="../colorWidgets/SAColorMenu.cpp" line="178"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../colorWidgets/SAColorMenu.cpp" line="212"/>
+        <source>Theme Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../colorWidgets/SAColorMenu.cpp" line="219"/>
+        <source>Custom Color</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonActionsManager</name>
+    <message>
+        <location filename="../SARibbonActionsManager.cpp" line="335"/>
+        <source>not in ribbon</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonActionsManagerModel</name>
+    <message>
+        <location filename="../SARibbonActionsManager.cpp" line="529"/>
+        <source>action name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonCategoryLayout</name>
+    <message>
+        <location filename="../SARibbonCategoryLayout.cpp" line="136"/>
+        <source>in SARibbonCategoryLayout cannot addItem,use addPanel instead</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonContextCategory</name>
+    <message>
+        <location filename="../SARibbonContextCategory.cpp" line="67"/>
+        <source>SARibbonContextCategory have category %1,will skip</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonCustomizeDialog</name>
+    <message>
+        <location filename="../SARibbonCustomizeDialog.cpp" line="51"/>
+        <source>Customize Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeDialog.cpp" line="52"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeDialog.cpp" line="53"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonCustomizeWidget</name>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="357"/>
+        <source>Customize Widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="358"/>
+        <source>Please Select</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="362"/>
+        <source>Find Command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="363"/>
+        <source>Add &gt;&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="364"/>
+        <source>&lt;&lt; Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="366"/>
+        <source>Customize the Ribbon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="367"/>
+        <source>Main Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="368"/>
+        <source>All Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="369"/>
+        <source>New Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="370"/>
+        <source>New Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="371"/>
+        <source>Rename</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="372"/>
+        <source>reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="373"/>
+        <source>proportion:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="768"/>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="771"/>
+        <source>large</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="769"/>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="773"/>
+        <source>small</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="772"/>
+        <source>medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="1154"/>
+        <source>new category[customize]%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="1179"/>
+        <source>new panel[customize]%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="1224"/>
+        <source>rename</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="1224"/>
+        <source>name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="1486"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="1487"/>
+        <source>Are you sure reset all customize setting?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonSystemButtonBar</name>
+    <message>
+        <location filename="../SARibbonSystemButtonBar.cpp" line="314"/>
+        <source>Restore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonSystemButtonBar.cpp" line="314"/>
+        <source>Maximize</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonTitleIconWidget</name>
+    <message>
+        <location filename="../SARibbonTitleIconWidget.cpp" line="135"/>
+        <source>Restore(R)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonTitleIconWidget.cpp" line="140"/>
+        <source>Move(M)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonTitleIconWidget.cpp" line="144"/>
+        <source>Size(S)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonTitleIconWidget.cpp" line="151"/>
+        <source>Minimize(N)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonTitleIconWidget.cpp" line="156"/>
+        <source>Maximize(X)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonTitleIconWidget.cpp" line="164"/>
+        <source>Close(C)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ src/SARibbonBar/i18n/SARibbon_zh_CN.ts
@@ -0,0 +1,281 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="336"/>
+        <source>SARibbon Warning !!! customize rename category,but get an empty category object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="360"/>
+        <source>SARibbon Warning !!! customize rename panel,but get an empty category/panel object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="383"/>
+        <source>SARibbon Warning !!! customize change category order,but get an empty category object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="407"/>
+        <source>SARibbon Warning !!! customize change panel order,but get an empty category/panel object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="436"/>
+        <source>SARibbon Warning !!! customize change action order,but get an empty category/panel/action object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="458"/>
+        <source>SARibbon Warning !!! customize remove category,but get an empty category object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="479"/>
+        <source>SARibbon Warning !!! customize remove panel,but get an empty category/panel object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="505"/>
+        <source>SARibbon Warning !!! customize remove action,but get an empty category/panel/action object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeData.cpp" line="527"/>
+        <source>SARibbon Warning !!! customize visible category,but get an empty category object name,if you want to customize SARibbon,please make sure every element has been set object name.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SAColorMenu</name>
+    <message>
+        <location filename="../colorWidgets/SAColorMenu.cpp" line="178"/>
+        <source>None</source>
+        <translation>无</translation>
+    </message>
+    <message>
+        <location filename="../colorWidgets/SAColorMenu.cpp" line="212"/>
+        <source>Theme Colors</source>
+        <translation>主题颜色</translation>
+    </message>
+    <message>
+        <location filename="../colorWidgets/SAColorMenu.cpp" line="219"/>
+        <source>Custom Color</source>
+        <translation>自定义颜色</translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonActionsManager</name>
+    <message>
+        <location filename="../SARibbonActionsManager.cpp" line="335"/>
+        <source>not in ribbon</source>
+        <translation>不在ribbon</translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonActionsManagerModel</name>
+    <message>
+        <location filename="../SARibbonActionsManager.cpp" line="529"/>
+        <source>action name</source>
+        <translation>动作名称</translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonCategoryLayout</name>
+    <message>
+        <location filename="../SARibbonCategoryLayout.cpp" line="136"/>
+        <source>in SARibbonCategoryLayout cannot addItem,use addPanel instead</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonContextCategory</name>
+    <message>
+        <location filename="../SARibbonContextCategory.cpp" line="67"/>
+        <source>SARibbonContextCategory have category %1,will skip</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonCustomizeDialog</name>
+    <message>
+        <location filename="../SARibbonCustomizeDialog.cpp" line="51"/>
+        <source>Customize Dialog</source>
+        <translation>自定义对话框</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeDialog.cpp" line="52"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeDialog.cpp" line="53"/>
+        <source>OK</source>
+        <translation>确定</translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonCustomizeWidget</name>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="357"/>
+        <source>Customize Widget</source>
+        <translation>自定义窗口</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="358"/>
+        <source>Please Select</source>
+        <translation>请选择</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="362"/>
+        <source>Find Command</source>
+        <translation>查找命令</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="363"/>
+        <source>Add &gt;&gt;</source>
+        <translation>添加&gt;&gt;</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="364"/>
+        <source>&lt;&lt; Remove</source>
+        <translation>&lt;&lt;移除</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="366"/>
+        <source>Customize the Ribbon</source>
+        <translation>自定义功能区</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="367"/>
+        <source>Main Category</source>
+        <translation>主选项卡</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="368"/>
+        <source>All Category</source>
+        <translation>所有选项卡</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="369"/>
+        <source>New Category</source>
+        <translation>新建选项卡</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="370"/>
+        <source>New Group</source>
+        <translation>新建组</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="371"/>
+        <source>Rename</source>
+        <translation>重命名</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="372"/>
+        <source>reset</source>
+        <translation>重置</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="373"/>
+        <source>proportion:</source>
+        <translation>比例:</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="768"/>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="771"/>
+        <source>large</source>
+        <translation>大</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="769"/>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="773"/>
+        <source>small</source>
+        <translation>小</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="772"/>
+        <source>medium</source>
+        <translation>中</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="1154"/>
+        <source>new category[customize]%1</source>
+        <translation>新选项卡[自定义]%1</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="1179"/>
+        <source>new panel[customize]%1</source>
+        <translation>新分组[自定义]%1</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="1224"/>
+        <source>rename</source>
+        <translation>重命名</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="1224"/>
+        <source>name:</source>
+        <translation>名字:</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="1486"/>
+        <source>Warning</source>
+        <translation>警告</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonCustomizeWidget.cpp" line="1487"/>
+        <source>Are you sure reset all customize setting?</source>
+        <translation>确认是否重置所有自定义设置?</translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonSystemButtonBar</name>
+    <message>
+        <location filename="../SARibbonSystemButtonBar.cpp" line="314"/>
+        <source>Restore</source>
+        <translation>恢复</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonSystemButtonBar.cpp" line="314"/>
+        <source>Maximize</source>
+        <translation>最大化</translation>
+    </message>
+</context>
+<context>
+    <name>SARibbonTitleIconWidget</name>
+    <message>
+        <location filename="../SARibbonTitleIconWidget.cpp" line="135"/>
+        <source>Restore(R)</source>
+        <translation>恢复(R)</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonTitleIconWidget.cpp" line="140"/>
+        <source>Move(M)</source>
+        <translation>移动(M)</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonTitleIconWidget.cpp" line="144"/>
+        <source>Size(S)</source>
+        <translation>大小(S)</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonTitleIconWidget.cpp" line="151"/>
+        <source>Minimize(N)</source>
+        <translation>最小化(N)</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonTitleIconWidget.cpp" line="156"/>
+        <source>Maximize(X)</source>
+        <translation>最大化(X)</translation>
+    </message>
+    <message>
+        <location filename="../SARibbonTitleIconWidget.cpp" line="164"/>
+        <source>Close(C)</source>
+        <translation>关闭(C)</translation>
+    </message>
+</context>
+</TS>
